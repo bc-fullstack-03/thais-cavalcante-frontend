6b9e6229e533ee27517bd542914a697b9e4f0189
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-05-15 23:54:00 -0300

    Initialize project

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+
+const tailwindColors = require("tailwindcss/colors");
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+    },
+    minWidth: {
+      login: "25rem",
+    },
+
+    colors: {
+      ...tailwindColors,
+      white: "#FFFFFF",
+      cyan: {
+        regular: "#81D8F7",
+        light: "#B9E5F4",
+      },
+      gray: {
+        dark: "#202024",
+        regular: "#7C7C8A",
+        light: "#E1E1E6",
+      },
+    },
+  },
+  plugins: [],
+};
