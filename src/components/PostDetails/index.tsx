c79f88ebf197236ad9cca94d8e1995e1227386cf
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-05-21 22:48:03 -0300

    Creating PostDetails component

---
+++ src/components/PostDetails/index.tsx
@@ -0,0 +1,50 @@
+import { useParams } from "react-router-dom";
+import PostItem from "../../components/PostItem";
+import { getAuthHeader } from "../../service/mainAPI/auth";
+import { useEffect, useState } from "react";
+import Heading from "../../components/Heading";
+import Comment from "../../components/Comment";
+import { getPost } from "../../service/mainAPI/post";
+import CreateComment from "../CreateComment";
+
+function PostDetails() {
+  const { id } = useParams();
+  const authHeader = getAuthHeader();
+  const [post, setPost] = useState<Post>(false as unknown as Post);
+
+  async function fetchPost() {
+    if (typeof id == "string") {
+      const post = await getPost(id, authHeader);
+      setPost(post);
+    }
+  }
+
+  useEffect(() => {
+    fetchPost();
+  }, []);
+
+  async function handlePostChanged() {
+    await fetchPost();
+  }
+
+  return (
+    <div className="basis-5/6 overflow-y-auto scroll-smooth">
+      <Heading className="pl-5 mt-4">Post</Heading>
+      {post && <PostItem post={post} />}
+      <CreateComment onCommentCreated={handlePostChanged} />
+      <Heading className="pl-5 mt-4">Comentários</Heading>
+      {post && post.comments.length > 0 && (
+        <>
+          {post.comments
+            .slice()
+            .reverse()
+            .map((comment) => (
+              <Comment comment={comment} />
+            ))}
+        </>
+      )}
+    </div>
+  );
+}
+
+export default PostDetails;
