ab4c522444259e9fae1a5e51664bcc2802b5892b
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-05-23 15:36:47 -0300

    Implementing disbled attribute to Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,13 +4,14 @@ interface ButtonProps {
   children: ReactNode;
   className?: string;
   onClick?: () => void;
+  disabled?: boolean;
 }
 
-function Button({ children, className, onClick }: ButtonProps) {
+function Button({ children, className, onClick, disabled }: ButtonProps) {
   const buttonClasses = `bg-cyan-regular h-10 font-inter text-md font-semibold transition-colors hover:bg-cyan-light rounded ${className}`;
 
   return (
-    <button className={buttonClasses} onClick={onClick}>
+    <button className={buttonClasses} onClick={onClick} disabled={disabled}>
       {children}
     </button>
   );
