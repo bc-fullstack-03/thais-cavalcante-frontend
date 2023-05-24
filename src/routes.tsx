9ca4a9794d7cb9e4a1047d23f114c57682c46b01
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-05-24 16:14:47 -0300

    Refactoring routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
@@ -9,61 +9,67 @@ import RequiredAuth from "./components/RequiredAuth";
 import Following from "./pages/Following";
 import Followers from "./pages/Followers";
 
-export default function AppRoutes() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route
-          path="/home"
-          element={
-            <RequiredAuth>
-              <Home />
-            </RequiredAuth>
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <RequiredAuth>
-              <Profile />
-            </RequiredAuth>
-          }
-        />
-        <Route
-          path="/friends"
-          element={
-            <RequiredAuth>
-              <Friends />
-            </RequiredAuth>
-          }
-        />
-        <Route
-          path="/post/:id"
-          element={
-            <RequiredAuth>
-              <PostPage />
-            </RequiredAuth>
-          }
-        />
-        <Route
-          path="/following/:id"
-          element={
-            <RequiredAuth>
-              <Following />
-            </RequiredAuth>
-          }
-        />
-        <Route
-          path="/followers/:id"
-          element={
-            <RequiredAuth>
-              <Followers />
-            </RequiredAuth>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
-  );
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/home",
+    element: (
+      <RequiredAuth>
+        <Home />
+      </RequiredAuth>
+    ),
+  },
+  {
+    path: "/profile",
+    element: (
+      <RequiredAuth>
+        <Profile />
+      </RequiredAuth>
+    ),
+  },
+  {
+    path: "/friends",
+    element: (
+      <RequiredAuth>
+        <Friends />
+      </RequiredAuth>
+    ),
+  },
+  {
+    path: "/post/:id",
+    element: (
+      <RequiredAuth>
+        <PostPage />
+      </RequiredAuth>
+    ),
+  },
+  {
+    path: "/following/:id",
+    element: (
+      <RequiredAuth>
+        <Following />
+      </RequiredAuth>
+    ),
+  },
+  {
+    path: "/followers/:id",
+    element: (
+      <RequiredAuth>
+        <Followers />
+      </RequiredAuth>
+    ),
+  },
+]);
+
+function AppRoutes() {
+  return <RouterProvider router={router} />;
 }
+
+export default AppRoutes;
